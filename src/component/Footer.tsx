a34adc295e3b1b8f6a209e09b89d20e391adaabf
= <iltk2000@gmail.com>
2022-10-19 21:46:47 +0900

    fix: blog url

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -28,8 +28,8 @@ export const Footer = (): JSX.Element | null => {
         <ul>
           <li>
             Github:{' '}
-            <a rel="noopener noreferrer" target="_blank" href="https://github.com/bluelion2">
-              https://github.com/bluelion2
+            <a rel="noopener noreferrer" target="_blank" href="https://bit.ly/3VGpzaw">
+              bit.ly/3VGpzaw
             </a>
           </li>
           <li>
